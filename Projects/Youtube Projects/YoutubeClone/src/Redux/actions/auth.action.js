b7ebe20f82c29deeb9bb066e04a27d3661e9295e
MadhuriBadgujar06 <badgujarmadhuri68@gmail.com>
2023-04-28 16:29:28 +0530

    Project Completed

--- Projects/Youtube Projects/YoutubeClone/src/Redux/actions/auth.action.js
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase/compat/app';
-import auth from '../../firebase';
-export const login =()=>async dispatch =>{
-    try{
-  const provider = new firebase.auth.GoogleAuthProvider()
- const res= await auth.signInWithPopup(provider);
- console.log(res)
-    }catch(error){}
-}
